5e6750fadb0597a40a6b3f4c8f3ab5f50c1ee62e
Vladimir Minkin <vladimir.minkin@gmail.com>
2022-11-20 20:34:23 +0300

    Writing tests

--- test/wire.test.js
+++ test/wire.test.js
@@ -4,16 +4,9 @@ import Wire from '../src/wire';
 class PutFindTestObject {}
 
 describe('1. Subscriptions', async () => {
-  const SIGNAL_G1 = 'SIGNAL';
-  // const SIGNAL_COUNTER = 'SIGNAL_COUNTER';
+  const SIGNAL_G1 = 'SIGNAL_G1';
+  const SIGNAL_COUNTER = 'SIGNAL_COUNTER';
   const SIGNAL_NOT_REGISTERED = 'SIGNAL_NOT_REGISTERED';
-
-  // const TEST_CASE_1_1 = '1.1. Has Signal';
-  // const TEST_CASE_1_2 = '1.2. Unregistered Signal';
-  // const TEST_CASE_1_3 = '1.3. Detach Signal';
-  // const TEST_CASE_1_4 = '1.4. Counter Signal with 2 replies';
-  // const TEST_CASE_1_5 = '1.5. Test put/find';
-  // const TEST_CASE_1_6 = '1.6. Add many and remove by scope';
 
   const SCOPE = Object();
   const print = console.log;
@@ -30,10 +23,10 @@ describe('1. Subscriptions', async () => {
 
   console.log('> 1: Setup -> Cleanup everything');
   await Wire.purge(true);
-  console.log('> 1: Setup -> Add signal $SIGNAL_G1 with dynamic WireListener');
+  console.log(`> 1: Setup -> Add signal ${SIGNAL_G1} with dynamic WireListener`);
   console.log('> \t\t Dynamic listener (with specified data type) will react on any signal');
   await Wire.add(SCOPE, SIGNAL_G1, listener_dynamic);
-  console.log('> 1: Setup -> Attach pre-created signal ${wireToAttach.signal} with string WireListener');
+  console.log(`> 1: Setup -> Attach pre-created signal ${wireToAttach.signal} with string WireListener`);
   Wire.attach(wireToAttach);
 
   test('1.0. Send registered signal', async () => {
@@ -54,9 +47,8 @@ describe('1. Subscriptions', async () => {
     print('> 1.2.1 -> Wire.send(SIGNAL_NOT_REGISTERED) == isTrue');
     expect((await Wire.send(SIGNAL_NOT_REGISTERED)).signalHasNoSubscribers).toBeTruthy();
     print('> 1.2.2 -> Wire.send("RANDOM") == isTrue');
-    expect((await Wire.send('RANDOM')).signalHasNoSubscribers).toBeTruthy();
+    expect((await Wire.send('RANDOM_SIGNAL')).signalHasNoSubscribers).toBeTruthy();
   });
-
   test('1.5. Test put/find', async () => {
     print('> TEST put/find instances by their type, the instance should be preserved');
     const putFindTestObject = new PutFindTestObject();
@@ -64,5 +56,49 @@ describe('1. Subscriptions', async () => {
     Wire.put(putFindTestObject);
     print(`> 1.5.2 -> Find object of type ${PutFindTestObject.name.toString()}`);
     expect(Wire.find(PutFindTestObject)).toBe(putFindTestObject);
+  });
+  test('1.6. Add many and remove by scope', async () => {
+    const scope = new PutFindTestObject();
+    const SIGNAL_NEW = 'SIGNAL_NEW';
+    const signalsToWireListeners = {
+      SIGNAL_G1: async () => {
+        print('> $TEST_CASE_1_6 -> Hello from $SIGNAL_G1');
+        return false;
+      },
+      SIGNAL_NEW: async () => {
+        print('> $TEST_CASE_1_6 -> Hello from $SIGNAL_NEW');
+        return SIGNAL_NEW;
+      },
+      SIGNAL_COUNTER: async () => {
+        print('> $TEST_CASE_1_6 -> Hello from $SIGNAL_COUNTER');
+        return 1;
+      },
+    };
+    await Wire.addMany(scope, new Map(Object.entries(signalsToWireListeners)));
+
+    print('> 1.6.1 -> Check if added signals exist:', Wire.get(SIGNAL_G1));
+    expect(Wire.get(SIGNAL_G1)).toHaveLength(2);
+    expect(Wire.get(SIGNAL_NEW)).toHaveLength(1);
+    expect(Wire.get(SIGNAL_COUNTER)).toHaveLength(1);
+
+    print('> 1.6.2 -> Send signal to verify their work');
+    expect((await Wire.send(SIGNAL_G1)).dataList).toHaveLength(2);
+    expect((await Wire.send(SIGNAL_G1)).dataList[1]).toBe(false);
+    expect((await Wire.send(SIGNAL_NEW)).dataList[0]).toBe(SIGNAL_NEW);
+    expect((await Wire.send(SIGNAL_COUNTER)).dataList[0]).toBe(1);
+
+    print('> 1.6.3 -> Call Wire.removeAllByScope(scope) -> existed == true');
+    expect(await Wire.remove(null, scope)).toBeTruthy();
+    expect(await Wire.remove(null, scope)).toBeFalsy();
+
+    print('> 1.6.4 -> Check no signals after removal - returned list is empty');
+    expect(Wire.get(null, scope)).toHaveLength(0);
+  });
+  test('1.7. Remove all by listener', async () => {
+    const wire = await Wire.add(new PutFindTestObject(), 'some_random_signal', listener_dynamic);
+    expect(Wire.has(wire.signal)).toBeTruthy();
+    expect(Wire.get(null, null, listener_dynamic)).toHaveLength(2);
+    expect(await Wire.remove(null, null, listener_dynamic)).toBeTruthy();
+    expect(Wire.get(null, null, listener_dynamic)).toHaveLength(0);
   });
 });
