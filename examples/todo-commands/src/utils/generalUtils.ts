ca4c103bf92232c0276a58e0068be4f82dddfbf9
Vladimir Minkin <vladimir.minkin@gmail.com>
2022-11-22 14:21:55 +0300

    Use latest version of library 0.0.31

--- examples/todo-commands/src/utils/generalUtils.ts
+++ examples/todo-commands/src/utils/generalUtils.ts
@@ -1,3 +1,7 @@
 export function utilRandomRange(max: number, min = 0): number {
   return Math.floor(Math.random() * (max - min) + min);
 }
+
+export function* utilFromObjectToEntities(input: { [index: string]: any }) {
+  for (const key in input) yield [key, input[key]];
+}
