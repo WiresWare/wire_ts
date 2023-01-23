c53af788d9a1d064115a4f44fa2e9f923ece50e5
Vladimir Minkin <vladimir.minkin@gmail.com>
2023-01-23 15:54:11 +0300

    CounterButton click async handler

--- examples/counter/src/app/counter/view/CounterButton.ts
+++ examples/counter/src/app/counter/view/CounterButton.ts
@@ -2,11 +2,11 @@ import { Wire } from 'cores.wire';
 
 class CounterButton {
   constructor(button: HTMLButtonElement, signal: string) {
-    button.onclick = () => {
+    button.onclick = async () => {
       button.disabled = true;
       // There is no payload since the signal defines
       // only operation (for general purpose button)
-      Wire.send(signal).finally(() => {
+      await Wire.send(signal).finally(() => {
         button.disabled = false;
       });
     };
