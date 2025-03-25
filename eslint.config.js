ccbbff3a93580325e9febb6f6ca1d01f7ca4890b
Vladimir Minkin <vladimir.minkin@gmail.com>
2025-03-25 07:01:48 +0000

    Update to latest dependencies, add bun.lock file, change eslint from json to config file.

---
+++ eslint.config.js
@@ -0,0 +1,38 @@
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        project: './tsconfig.json',
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
+    },
+    rules: {
+      '@typescript-eslint/ban-ts-comment': 0,
+      '@typescript-eslint/no-non-null-assertion': 0,
+      '@typescript-eslint/no-unused-vars': 'error',
+      '@typescript-eslint/explicit-function-return-type': 0,
+      '@typescript-eslint/no-explicit-any': 0,
+    },
+  },
+  {
+    plugins: {
+    },
+    rules: {
+      'quotes': ['error', 'single', { allowTemplateLiterals: true }],
+      'comma-dangle': ['error', 'always-multiline'],
+      'semi': ['error', 'always'],
+      'no-trailing-spaces': 'error',
+    },
+    linterOptions: {
+      reportUnusedDisableDirectives: true,
+    }
+  }
+);
